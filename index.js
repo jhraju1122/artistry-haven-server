63f5a8a2035040b4a7faf43b03af6d09bb8c8ca0
jhraju <jhraju890@gmail.com>
2024-11-07 18:51:43 +0600

    added deleted count operation success

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
- const { MongoClient, ServerApiVersion } = require('mongodb');
+ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,6 +36,16 @@ async function run() {
     await client.connect();
 
     const craftCollection =client.db('craftDB').collection('craft');
+    
+    // read operation 
+    app.get('/craft', async(req, res) =>{
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    // create 
     app.post('/craft', async(req, res) =>{
       const addItems = req.body;
       console.log(addItems);
@@ -43,6 +53,13 @@ async function run() {
       res.send(result);
     })
 
+    // delete operation
+    app.delete('/craft/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
